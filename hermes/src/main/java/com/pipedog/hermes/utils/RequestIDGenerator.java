be267ccb41b4c00b5cf30a5d54155beed40883a0
liang <leiliang4@xdf.cn>
2022-06-24 15:18:22 +0800

    requestID 线程保护

--- hermes/src/main/java/com/pipedog/hermes/utils/RequestIDGenerator.java
+++ hermes/src/main/java/com/pipedog/hermes/utils/RequestIDGenerator.java
@@ -1,6 +1,7 @@
 package com.pipedog.hermes.utils;
 
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author liang
@@ -9,14 +10,14 @@ import java.util.Date;
  */
 public class RequestIDGenerator {
 
-    private static long counter = 0;
+    private static AtomicLong counter = new AtomicLong(1);
 
     /**
      * 获取 requestID
      */
     public static String getRequestID() {
         Date date = new Date();
-        String requestId = String.format("[%s]::[%d]", date.toString(), counter++);
+        String requestId = String.format("[%s]::[%d]", date.toString(), counter.getAndIncrement());
         return requestId;
     }
 
